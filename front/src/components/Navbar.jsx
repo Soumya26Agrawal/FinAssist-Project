fac1756ad11dbc7367909a4bf16324c5ef15a631
Soumya26Agrawal <129431166+Soumya26Agrawal@users.noreply.github.com>
2025-03-29 00:05:30 +0530

    Update Navbar.jsx

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useFirebase } from "../config/firebase";
+import { useFirebase } from "../config/Firebase";
 function Navbar() {
   const { signout } = useFirebase();
   return (
