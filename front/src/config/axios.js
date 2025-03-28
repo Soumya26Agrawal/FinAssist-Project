2fd1b141ef02aa0c907f13eb62fc2c246dd345eb
Soumya26Agrawal <129431166+Soumya26Agrawal@users.noreply.github.com>
2025-03-29 00:27:20 +0530

    Update axios.js

--- front/src/config/axios.js
+++ front/src/config/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosIns1 = axios.create({
-  baseURL: "https://finassist-project-zmh4.onrender.com/app/v1/users",
+  baseURL: "https://finassist-project-sg71.onrender.com/app/v1/users",
   withCredentials: true,
 });
 const axiosIns2 = axios.create({
-  baseURL: "https://finassist-project-zmh4.onrender.com/app/v1/finances",
+  baseURL: "https://finassist-project-sg71.onrender.com/app/v1/finances",
   withCredentials: true,
 });
 
